95e0c2dba73ebf4b6028673331eb6541861bba17
Jackred <jackred@tuta.io>
2020-11-05 23:04:18 +0000

    ADD basic command for testing

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
 const login = require('facebook-chat-api');
 const config = require('./config.json');
 
+const cmdArrays = { github: buildGithubURI };
+
 // Create simple echo bot
 login({ email: config.email, password: config.password }, (err, api) => {
   if (err) {
@@ -16,8 +18,41 @@ login({ email: config.email, password: config.password }, (err, api) => {
     return;
   }
 
+  api.setOptions({ selfListen: true });
+
   api.listenMqtt((err, message) => {
+    if (!('body' in message)) {
+      return;
+    }
     console.log(message);
-    api.sendMessage('Vous avez dit: ' + message.body, message.threadID);
+    const cmdName = parseCommand(message.body);
+    if (cmdName !== null) {
+      let res = callFunction(message.body, cmdName);
+      api.sendMessage(res, message.threadID);
+    }
   });
 });
+
+function parseCommand(text) {
+  let command = null;
+  if (text.startsWith(config.prefix)) {
+    const cmdToParse = text.split(' ')[0].replace(config.prefix, '');
+    if (cmdToParse in cmdArrays) {
+      command = cmdToParse;
+    }
+  }
+  return command;
+}
+
+function buildGithubURI(text) {
+  const baseURI = 'https://github.com/';
+  const splitted = text.split(' ');
+  const user = splitted[0];
+  const repo = splitted.length > 1 ? splitted[1] : '';
+  return baseURI + user + '/' + repo;
+}
+
+function callFunction(text, cmdName) {
+  text = text.replace(config.prefix + cmdName, '').trim();
+  return cmdArrays[cmdName](text);
+}
